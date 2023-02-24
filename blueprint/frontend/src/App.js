61390b60d458e9110cbac760c16447cb07dfdcaa
Man Hei Ho <man.ho@mail.utoronto.ca>
2023-02-23 21:02:48 -0500

    feat: some time code for yjs

--- blueprint/frontend/src/App.js
+++ blueprint/frontend/src/App.js
@@ -5,6 +5,9 @@ import "./App.css";
 import { createUser, getMe } from "./reducers/userReducer";
 import TestReactFlow from "./test";
 
+import * as Y from 'yjs';
+import { WebsocketProvider } from 'y-websocket'
+
 function App() {
   const dispatch = useDispatch();
 
@@ -25,6 +28,26 @@ function App() {
       })
     );
   }
+
+  ///////////////////////////////////////
+  const ydoc = new Y.Doc()
+
+  // Sync clients with the y-websocket provider
+  const websocketProvider = new WebsocketProvider(
+    'ws://localhost:3002', 'test-room', ydoc
+  )
+
+  // array of numbers which produce a sum
+  const yarray = ydoc.getArray('count')
+
+  // observe changes of the sum
+  yarray.observe(event => {
+    // print updates when the data changes
+    console.log('new sum: ' + yarray.toArray().reduce((a, b) => a + b))
+  })
+
+  // add 1 to the sum
+  yarray.push([1]) // => "new sum: 1"
 
   return (
     <div class="test-react">
