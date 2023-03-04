bcb3eb2d8b921d7ec75eb55eeb2e7d060e07b3d6
Man Hei Ho <man.ho@mail.utoronto.ca>
2023-03-04 16:47:25 -0500

    feat: implemented basic synchronization

--- blueprint/frontend/src/App.js
+++ blueprint/frontend/src/App.js
@@ -1,58 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import "./App.css";
 
-import { createUser, getMe } from "./reducers/userReducer";
 import TestReactFlow from "./test";
 
-import * as Y from "yjs";
-import { WebsocketProvider } from "y-websocket";
-
 function App() {
-  const dispatch = useDispatch();
-
-  const user = useSelector((state) => state.user);
-
-  useEffect(() => {
-    dispatch(getMe());
-  }, [dispatch]);
-
-  useEffect(() => {
-    user && console.log(user);
-  }, [user]);
-
-  function dispatchCreateUser() {
-    dispatch(
-      createUser({
-        username: "test",
-      })
-    );
-  }
-
-  ///////////////////////////////////////
-  const ydoc = new Y.Doc();
-
-  // Sync clients with the y-websocket provider
-  const websocketProvider = new WebsocketProvider(
-    "ws://localhost:3002",
-    "test-room",
-    ydoc
-  );
-
-  // array of numbers which produce a sum
-  const yarray = ydoc.getArray("count");
-
-  // observe changes of the sum
-  yarray.observe((event) => {
-    // print updates when the data changes
-    console.log("new sum: " + yarray.toArray().reduce((a, b) => a + b));
-  });
-
-  // add 1 to the sum
-  yarray.push([1]); // => "new sum: 1"
-
   return (
-    <div class="test-react">
+    <div className="test-react">
       <TestReactFlow></TestReactFlow>
     </div>
   );
--- blueprint/frontend/src/test.js
+++ blueprint/frontend/src/test.js
@@ -1,7 +1,6 @@
 import "reactflow/dist/style.css";
 
 import { useState, useCallback, useEffect } from "react";
-import { useDispatch } from "react-redux";
 import ReactFlow, {
   Controls,
   Background,
@@ -10,12 +9,27 @@ import ReactFlow, {
   addEdge,
 } from "reactflow";
 
-import { updateNode } from "./reducers/nodeReducer";
-import { updateEdge } from "./reducers/edgeReducer";
+import * as Y from "yjs";
+import { WebsocketProvider } from "y-websocket";
+
+// this specifies the websocket server
+const HOST = "localhost";
+const PORT = "3002";
+const WEBSOCKET_URL = "ws://" + HOST + ":" + PORT;
+
+// initial document
+const ydoc = new Y.Doc();
+
+// ymap
+const elementMap = ydoc.getMap("element-map");
+
+// *HARD CODED FOR NOW* unique room id
+const roomId = "test-id";
 
 const TestReactFlow = () => {
-  const dispatch = useDispatch();
-
+  /*
+    Initial document setup.
+  */
   const initialNodes = [
     {
       id: "1",
@@ -33,27 +47,70 @@ const TestReactFlow = () => {
 
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
+  const [nodesChanged, setNodesChanged] = useState(false);
+  const [edgesChanged, setEdgesChanged] = useState(false);
+
+  /* 
+    Connect to y-websocket provider with YJS document and unique room id.
+    Set up observer
+  */
+  useEffect(() => {
+    // make websocket server connection on mount
+    const websockerProvider = new WebsocketProvider(
+      WEBSOCKET_URL,
+      roomId,
+      ydoc
+    );
+    console.log("Connected to YJS websocket provider.");
+
+    // set up observer
+    elementMap.observe((event) => {
+      console.log("observed");
+      setNodes(elementMap.get("nodes"));
+      setEdges(elementMap.get("edges"));
+    });
+
+    // cleanup function to disconnect from websocket when unmount
+    return () => {
+      websockerProvider.destroy();
+    };
+  }, []);
+
+  /*
+    Publishers. 
+    Only publish if this user made changes.
+  */
+  useEffect(() => {
+    if (nodesChanged) {
+      elementMap.set("nodes", nodes);
+      setNodesChanged(false);
+    }
+  }, [nodesChanged, nodes]);
 
   useEffect(() => {
-    dispatch(updateNode(nodes));
-  }, [dispatch, nodes]);
+    if (edgesChanged) {
+      elementMap.set("edges", edges);
+      setEdgesChanged(false);
+    }
+  }, [edgesChanged, edges]);
 
-  useEffect(() => {
-    dispatch(updateEdge(edges));
-  }, [dispatch, edges]);
+  /*
+    Callbacks to handle changes made by this user.
+  */
+  const onNodesChange = useCallback((changes) => {
+    setNodes((nds) => applyNodeChanges(changes, nds));
+    setNodesChanged(true);
+  }, []);
 
-  const onNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    []
-  );
-  const onEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    []
-  );
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    []
-  );
+  const onEdgesChange = useCallback((changes) => {
+    setEdges((eds) => applyEdgeChanges(changes, eds));
+    setEdgesChanged(true);
+  }, []);
+
+  const onConnect = useCallback((params) => {
+    setEdges((eds) => addEdge(params, eds));
+    setEdgesChanged(true);
+  }, []);
 
   return (
     <div style={{ height: "100%" }}>
