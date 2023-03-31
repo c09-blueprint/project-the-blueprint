aa35cec066a9384f03770b6dd485bffd4377d244
Man Hei Ho <man.ho@mail.utoronto.ca>
2023-03-31 19:06:39 -0400

    chore: code cleanup and added frontend auth

--- blueprint/frontend/src/services/userService.js
+++ blueprint/frontend/src/services/userService.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 import { ENDPOINTS } from "./endpoints";
-import GenericService from "./service";
 import { getAuthHeader } from "../utils/authService";
-
-const userService = new GenericService(ENDPOINTS.BASE_USER_URL);
 
 // get logged in user
 const me = async (email, token) => {
