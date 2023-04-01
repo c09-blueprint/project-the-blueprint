73cc8d10ba80c1d342973e608fff1154e07b1024
Man Hei Ho <man.ho@mail.utoronto.ca>
2023-03-31 21:12:02 -0400

    chore: added docker files

--- blueprint/backend/datasource.js
+++ blueprint/backend/datasource.js
@@ -3,13 +3,4 @@ import { config } from "dotenv";
 
 config();
 
-const dbConfig = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: "postgres",
-};
-
-export const sequelize = new Sequelize(dbConfig);
+export const sequelize = new Sequelize(process.env.DATABASE_URL);
