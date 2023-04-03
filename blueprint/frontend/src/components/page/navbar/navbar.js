6c64e55d8b9c71c4b7917bd829ce694231bdefef
Christina Ma <christina.cm.ma@gmail.com>
2023-04-03 15:56:36 -0400

    chore: made navbar responsive

--- blueprint/frontend/src/components/page/navbar/navbar.js
+++ blueprint/frontend/src/components/page/navbar/navbar.js
@@ -3,15 +3,16 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { LogoutButton } from "../../buttons/logoutButton";
 import { LoginButton } from "../../buttons/loginButton";
 import { SignupButton } from "../../buttons/signupButton";
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  /* Conditionally rendering signin/login/logout button */
   const { isAuthenticated } = useAuth0();
+  const location = useLocation();
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark menubar">
-      <a className="navbar-brand mb-0 h1 bp-logo" href="\home">
+      <a className="navbar-brand mb-0 h1 bp-logo" href="\">
         Blueprint
       </a>
       <button
@@ -28,13 +29,21 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         {isAuthenticated && (
           <ul className="navbar-nav menu-item-padding">
-            <li className="nav-item active">
-              <a className="nav-link" href="\dashboard">
+            <li
+              className={`nav-item ${
+                location.pathname === "/dashboard" ? "active" : ""
+              }`}
+            >
+              <a className="nav-link" href="\dashboard" id="workspace">
                 My Workspace
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/dashboard/shared">
+            <li
+              className={`nav-item ${
+                location.pathname === "/dashboard/shared" ? "active" : ""
+              }`}
+            >
+              <a className="nav-link" href="/dashboard/shared" id="shared">
                 Shared With Me
               </a>
             </li>
@@ -53,9 +62,6 @@ const Navbar = () => {
                 className="dropdown-menu"
                 aria-labelledby="navbarDropdownMenuLink"
               >
-                <a className="dropdown-item" href="#">
-                  Workspace
-                </a>
                 <button
                   type="button"
                   data-toggle="modal"
@@ -74,23 +80,19 @@ const Navbar = () => {
               <li className="nav-item">
                 <SignupButton />
               </li>
-            </ul>
-            <ul className="navbar-nav ms-auto menu-item-padding">
               <li className="nav-item">
                 <LoginButton />
               </li>
             </ul>
           </>
         )}
-        <ul className="navbar-nav ms-auto menu-item-padding">
-          {isAuthenticated && (
-            <ul className="navbar-nav ms-auto menu-item-padding">
-              <li className="nav-item">
-                <LogoutButton />
-              </li>
-            </ul>
-          )}
-        </ul>
+        {isAuthenticated && (
+          <ul className="navbar-nav ms-auto menu-item-padding">
+            <li className="nav-item">
+              <LogoutButton />
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
